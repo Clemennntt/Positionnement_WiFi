eb668a77d9ed33019f6127f0f3168c54292dbdd8
erenault <erw.renault@gmail.com>
2023-06-18 17:05:43 -0400

    Attention c'est tout cassé

--- traitement_data.py
+++ traitement_data.py
@@ -1,6 +1,95 @@
+#! /usr/bin/python3
 import pandas as pd
 from scipy.stats import chi2_contingency
 from sklearn.preprocessing import OneHotEncoder
+import sys
+
+def _mac_to_int(mac_address):
+    """
+    Permet de coder les SA (string) en valeur numérique (int) en conservant toute l'information 
+    """
+    hex_parts = mac_address.split(':')
+    hex_string = ''.join(hex_parts)
+    return int(hex_string, 16)
+
+def codage_one_hot_for_ML(data) :
+    """
+    Permet de coder les SA en one hot vu que c'est pas des valeurs numérique
+    """
+    encoder = OneHotEncoder()
+
+    # Calcul du nbr de SA diff
+    nombre_sa_differentes = data['SA'].nunique()
+    print('--------------------')
+    print("Nombre de SA différentes dans le dataset :", nombre_sa_differentes)
+
+    # Ajout des données en one hot avec concatenate
+    encoded_SA = encoder.fit_transform(data[['SA']]).toarray()
+    data = pd.concat([data, pd.DataFrame(encoded_SA)], axis=1)
+    
+    # Affichage du new dataset
+    print(data.head(3))
+
+    return data
+
+def _extract_clean_dataset(filename):
+    """
+    Charge un fichier et le nettoie. (sauf les non utf8 genre les emoji ça faut le faire à la main avec libre calc psk jsp pk ça marche pô)
+    """
+    df = pd.read_csv(filename)
+    df.columns = df.columns.map(lambda x: x.strip())
+    
+    return df
+
+def _dataset_groupby_saxyz(df):
+    """
+    Réduit la quantitée de données et fait le tri des colonnes à garder
+    """
+    df2 = df.groupby(['SA', 'x', 'y', 'z']).aggregate({
+        'RSSI': 'mean',
+        'COUNT': 'sum',
+        'SSID': 'unique', 
+        'BSSID': 'unique', 
+        'DA': 'unique', 
+        'antenna_index': 'unique', 
+        'channel': 'unique', 
+        'TSFT': 'unique',
+        'flags': 'unique', 
+        'data_rate': 'unique', 
+        'rx_flag': 'unique', 
+        'timestamp': 'median',
+        'mcs': 'unique',
+        'ampdu_status': 'unique',
+    }).reset_index()
+
+    return df2
+
+def pivot_dataset(df):
+    """
+    Fait pivoter la table pour avoir xy en première colonne puis chaque SA en colonne (et ajoute -99 pour les vals manquantes)
+    """
+    copy = df.copy()
+    copy['SA'] = copy['SA'].apply(_mac_to_int)
+
+    pivoted = pd.pivot_table(copy, values='RSSI', index=['x', 'y'], columns=['SA'], fill_value=-99).reset_index()
+
+    return pivoted
+
+def prepare_data(train_set_path, test_set_path):
+    """
+    Réalisé toute les opération de netoyage et de tri. Les datasets peuvent alors être utilisés
+    """
+    # load datasets and clean data if needed (A manual deletion of non UTF-8 char should be done manually since pandas doesn't seems to find them)
+    df_train = _extract_clean_dataset(train_set_path)
+    df_test = _extract_clean_dataset(test_set_path)
+
+    df_train_cleaned, df_test_cleaned = convert_same_SA(df_train, df_test)
+
+    # perform data reduction
+    df_train_converted = _dataset_groupby_saxyz(df_train_cleaned)
+    df_test_converted = _dataset_groupby_saxyz(df_test_cleaned)
+
+    return df_train_converted, df_test_converted
 
 def convert_same_SA(data1, data2) :
     """
@@ -14,15 +103,10 @@ def convert_same_SA(data1, data2) :
     inter_SA = data1_SA.intersection(data2_SA)
 
     # Filtrage des dataset avec les inter_SA
-    data1 = data1[data1['SA'].isin(inter_SA)]
-    data2 = data2[data2['SA'].isin(inter_SA)]
+    data1_final = data1[data1['SA'].isin(inter_SA)]
+    data2_final = data2[data2['SA'].isin(inter_SA)]
 
-    # Affichage de la tail (voir si c'est le même et voir la diff de nbr total de SA)
-    print('Voici la conversion avec les mm SA :')
-    print(data1.tail(5))
-    print(data2.tail(5))
-
-    return data1, data2
+    return data1_final, data2_final
 
 def filtrage_colonne_for_ML(data) :
     """
@@ -81,22 +165,35 @@ def khi2(data) :
     #         print("p-value :", p_value)
     #         print("")
 
-def codage_one_hot_for_ML(data) :
+
+
+
+
+def main(argc, argv):
     """
-    Permet de coder les SA en one hot vu que c'est pas des valeurs numérique
+    Fonction si le fichier et run directement. Si aucun params ne sont passés, des fichiers par défaut sont chargés
     """
-    encoder = OneHotEncoder()
+    train_set_path = 'salle_P0_16_06_A3_ready_to_use.csv'
+    test_set_path = 'salle_P0_16_06_B3_ready_to_use.csv'
+    if argc >= 3:
+        train_set_path = argv[1]
+        test_set_path = argv[2]
 
-    # Calcul du nbr de SA diff
-    nombre_sa_differentes = data['SA'].nunique()
-    print('--------------------')
-    print("Nombre de SA différentes dans le dataset :", nombre_sa_differentes)
+    # perform data reduction
+    df_train_converted, df_test_converted = prepare_data(train_set_path, test_set_path)
 
-    # Ajout des données en one hot avec concatenate
-    encoded_SA = encoder.fit_transform(data[['SA']]).toarray()
-    data = pd.concat([data, pd.DataFrame(encoded_SA)], axis=1)
-    
-    # Affichage du new dataset
-    print(data.head(3))
+    #df_train_converted.to_csv(path_or_buf="train_set.csv",index=False)
+    #df_test_converted.to_csv(path_or_buf="test_set.csv",index=False)
 
-    return data
+    df_train_pivoted = pivot_dataset(df_train_converted)
+    df_test_pivoted = pivot_dataset(df_test_converted)
+
+    df_train_pivoted.to_csv(path_or_buf="train_set_pivoted.csv",index=False)
+    df_test_pivoted.to_csv(path_or_buf="test_set_pivoted.csv",index=False)
+
+if __name__ == "__main__":
+    """
+    Pas toucher =3
+    """
+    argc = len(sys.argv)
+    main(argc, sys.argv)
--- tree.py
+++ tree.py
@@ -1,44 +1,64 @@
 #! /usr/bin/python3
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import re
 import sys
+import math
 from sklearn import tree
+from traitement_data import prepare_data, pivot_dataset, _extract_clean_dataset, convert_same_SA
 
-def extract_clean_dataset(filename):
-    df = pd.read_csv(filename)
-    df.columns = df.columns.map(lambda x: x.strip()) #remove spaces, tabs, etc, that could be produced by term
-    return df
+
+def train_model(df_train):
+    df_train_pivoted = pivot_dataset(df_train)
+
+    X = df_train_pivoted.drop(['x', 'y'], axis=1)
+    Y = df_train_pivoted[['x', 'y']].to_numpy()
+    clf = generate_model(X, Y)
+
+    return clf
 
 def generate_model(X, Y):
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(X, Y)
     return clf
 
-def test_model(clf, Y):
-    return clf.predict(Y)
+def test_model(clf, df_test):
+    df_test_pivoted = pivot_dataset(df_test)
+    X = df_test_pivoted.drop(['x', 'y'], axis=1)
+    Y = df_test_pivoted[['x', 'y']].to_numpy()
+
+    y = clf.predict(X)
+    dist = [] 
+    for i in range(0, len(y)):
+        dist.append(math.sqrt((y[i][0] - Y[i][0])**2 + (y[i][1] - Y[i][1])**2))
+    
+    print(np.mean(dist))
+
+    return y
+
+def estimate_position(clf, X):
+    y = clf.predict_proba(X)
+    return y
 
 
-def main():
-    argc = len(sys.argv)
+def main(argc, argv):
     if argc < 2:
         print("Please provide at least one file")
         exit(1)
+
+    df_train_cleaned, df_test_cleaned = prepare_data(argv[1], argv[2])
     
-    df_train = extract_clean_dataset(sys.argv[1])
-    X = df_train.drop(['x', 'y'], axis=1)
-    Y = df_train[['x', 'y']]
-    clf = generate_model(X, Y)
+    clf = train_model(df_train_cleaned)
+    
+    test_model(clf, df_test_cleaned)
 
-    #print(X[-1:])
-    df_test = extract_clean_dataset(sys.argv[2])
-    X = df_test.drop(['x', 'y'], axis=1)
-    Y = df_test[['x', 'y']]
-
-    x = test_model(clf, X)
-    print(x)
-    #print(Y)
+    df_test_pivoted = pivot_dataset(df_test_cleaned)
+    #X = df_test_pivoted.drop(['x', 'y'], axis=1)
+    #print(X.iloc[0])
+    #pred = estimate_position(clf, X.iloc[0])
+    #print(pred)
 
 
 if __name__ == "__main__":
-    main()
+    argc = len(sys.argv)
+    main(argc, sys.argv)
