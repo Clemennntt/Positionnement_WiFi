f6fb9d9189408833060ebc6cc0bb17cf3d0c311d
Clemennntt <clement.loiseau@student.junia.com>
2023-06-18 11:29:33 -0400

    Update

--- KNN.py
+++ KNN.py
@@ -4,46 +4,85 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from traitement_data import convert_same_SA, filtrage_colonne_for_ML, correlation_Pearson, correlation_Spearman, khi2
+from scipy.stats import chi2_contingency
 
-#### Préparation des données ####
+def test_correlation(data) :
+    """ 
+    On fait les différents tests
+    """
+    correlation_Pearson(data_train)
+    correlation_Spearman(data_train)
+    khi2(data_train)
 
-# Charge dataset 
-dataset = pd.read_csv('fichier.csv')
+def KNN(data_train, data_test) :
+    """
+    Programme KNN
+    """
 
-# Visualisation (comme tu as fait avec les couleurs ???)
-##A RAJOUTER##
+    # # Split the data
+    # X = data_train[['position_x', 'position_y', 'adresse_mac']]
+    # y = data_train['DB_mean']
 
-# Split the data
-X = dataset[['position_x', 'position_y', 'adresse_mac']]
-y = dataset['DB_mean']
+    # # Sélection des colonnes à normaliser et normalisation
+    # columns_to_normalize = ['position_x', 'position_y']
+    # scaler = StandardScaler()
+    # X[columns_to_normalize] = scaler.fit_transform(X[columns_to_normalize])
 
-# Sélection des colonnes à normaliser et normalisation
-columns_to_normalize = ['position_x', 'position_y']
-scaler = StandardScaler()
-X[columns_to_normalize] = scaler.fit_transform(X[columns_to_normalize])
+    # # Split en test et train
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Split en test et train
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # #### Modèle et prédiction ####
 
-#### Modèle et prédiction ####
+    # # KNN model & prédiction
+    # knn = KNeighborsRegressor(n_neighbors=3)
+    # knn.fit(X_train, y_train)
+    # y_pred = knn.predict(X_test)
 
-# KNN model & prédiction
-knn = KNeighborsRegressor(n_neighbors=3)
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
+    # #### Evaluation ####
 
-#### Evaluation ####
+    # # Métriques d'éval (cf TP1)
+    # mae = mean_absolute_error(y_test, y_pred)
+    # rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    # r2 = r2_score(y_test, y_pred)
 
-# Métriques d'éval (cf TP1)
-mae = mean_absolute_error(y_test, y_pred)
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-r2 = r2_score(y_test, y_pred)
-
-print("Mean Absolute Error (MAE):", mae)
-print("Root Mean Square Error (RMSE):", rmse)
-print("R² Score:", r2)
+    # print("Mean Absolute Error (MAE):", mae)
+    # print("Root Mean Square Error (RMSE):", rmse)
+    # print("R² Score:", r2)
 
 
-# Affichaged es prédictions
-predictions = pd.DataFrame({'Position_x': X_test['position_x'], 'Position_y': X_test['position_y'], 'Adresse_MAC': X_test['adresse_mac'], 'DB_mean_prédit': y_pred})
-print(predictions)
+    # # Affichaged es prédictions
+    # predictions = pd.DataFrame({'Position_x': X_test['position_x'], 'Position_y': X_test['position_y'], 'Adresse_MAC': X_test['adresse_mac'], 'DB_mean_prédit': y_pred})
+    # print(predictions)
+
+
+#========================================================================================================================================#
+#================================================================= Main =================================================================#
+#========================================================================================================================================#
+
+if __name__ == "__main__":
+    
+    #### Préparation des données ####
+
+    # Import les données
+    data_test = pd.read_csv('salle_P0_16_06_A3_ready_to_use.csv')
+    data_train = pd.read_csv('salle_P0_16_06_B3_ready_to_use.csv')
+
+    # Filtrage des datas 
+    data_train, data_test = convert_same_SA(data_train, data_test)
+
+    ## A PARTIR D'ICI DATA_TRAIN et DATA_TEST SONT LES BASES POUR LES 3 PROGRAMMES (MLx + DL) ##
+
+    #### Programme Machine Learning ####
+    #### Filtrage pour ML ####
+
+    # On garde les colonnes essentiels : SA, COUNT, x, y, z
+    data_train_ML = filtrage_colonne_for_ML(data_train)
+    data_test_ML = filtrage_colonne_for_ML(data_test)
+
+    # Tests de corrélation
+    test_correlation(data_train_ML)
+
+    #### Programme 1 : KNN #### 
+
+    KNN(data_train_ML, data_test_ML)
---
+++ traitement_data.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+def convert_same_SA(data1, data2) :
+    """
+    Permet de filtrer les 2 datasets en gardant uniquement les mêmes SA
+    """
+    # Set contenant les valeurs uniques de SA
+    data1_SA = set(data1['SA'])
+    data2_SA = set(data2['SA'])
+
+    # data1_SA ∩ data2_SA
+    inter_SA = data1_SA.intersection(data2_SA)
+
+    # Filtrage des dataset avec les inter_SA
+    data1 = data1[data1['SA'].isin(inter_SA)]
+    data2 = data2[data2['SA'].isin(inter_SA)]
+
+    # Affichage de la tail (voir si c'est le même et voir la diff de nbr total de SA)
+    print('Voici la conversion avec les mm SA :')
+    print(data1.tail(5))
+    print(data2.tail(5))
+
+    return data1, data2
+
+def filtrage_colonne_for_ML(data) :
+    """
+    Permet de garder les colonnes utiles pour les programmes ML
+    SA, COUNT, x, y, z
+    """
+    # Selection des colonnes
+    colonnes_useful = ['x', 'y', 'z', 'SA', 'RSSI']
+    data = data[colonnes_useful]
+
+    # Afficahge de la head voir le tableau
+    print('\nVoici le filtrage avec les colonnes utiles pour le ML :')
+    print(data.head(5))
+    return data
+
+def correlation_Pearson(data) :
+    """
+    Permet de faire le test de corrélation de Pearson(linéaire) pour voir la prédictibilité
+    """
+    # Calcule de la correlation et affichage (Pearson uniquement pour les val numériques)
+    correlation = data.corr()
+    print('\nVoici le test de correlation de Pearson :')
+    print(correlation)
+
+def correlation_Spearman(data) :
+    """
+    Permet de faire le test de corrélation de Spearman (monotone) pour voir la prédictibilité
+    """
+    # Calcule de la correlation et affichage (Sspearman uniquement pour les val numériques)
+    correlation = data.corr(method='spearman')
+    print('\nVoici le test de correlation de Spearman:')
+    print(correlation)
+
+def khi2(data) :
+    """
+    Permet de test le khi2 entre SA et chaque autre variable catégorielle
+    """
+    #Test entre respectivement x, y, z et SA par rapport à RSSI
+    for colonne in ['x', 'y', 'z']:
+        tableau_contingence = pd.crosstab(data[colonne], [data['SA'], data['RSSI']])
+        chi2, p_value, dof, expected = chi2_contingency(tableau_contingence)
+        print(f"\nTest khi2 entre {colonne} et SA par rapport à RSSI:")
+        print("Tableau de contingence :")
+        print(tableau_contingence)
+        print("p-value :", p_value)
+        print("")
+
+    # Ancien test entre SA et x, y, z, RSSI mais faible corrélation et PAS PERTINENT
+    # for colonne in data.columns:
+    #     if colonne != 'SA':
+    #         tableau_contingence = pd.crosstab(data['SA'], data[colonne])
+    #         chi2, p_value, dof, expected = chi2_contingency(tableau_contingence)
+    #         print(f"Test khi2 entre SA et '{colonne}':")
+    #         print("Tableau de contingence :")
+    #         print(tableau_contingence)
+    #         print("p-value :", p_value)
+    #         print("")
