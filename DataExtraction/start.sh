6efdc96802022899a27dd3b869fb7b80350e7950
erenault <erw.renault@gmail.com>
2023-10-28 12:57:27 +0200

    Update project

--- DataExtraction/start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo service NetworkManager stop
-sudo ip link set wlp2s0 down
-sudo iwconfig wlp2s0 mode monitor
-sudo ip link set wlp2s0 up
--- DataExtraction/stop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo ip link set wlp2s0 down
-sudo iwconfig wlp2s0 mode managed
-sudo ip link set wlp2s0 up
-sudo service NetworkManager start
